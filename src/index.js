4574bd34a9fada922579f84b3c9ac268f2798b21
maxevilmind <larionov_m@protonmail.com>
2025-05-21 21:11:24 +0200

    chore: remove unused files and components, streamline project structure for better maintainability

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// import './styles/tailwind.css';
-import './components/portfolio-app.js';
-import './components/home-page.js';
-import './components/projects-page.js';
-import './components/about-page.js';
-import './components/contact-page.js'; 
