775593889119ecfcd308cdd59d33dc21386dc01b
maxevilmind <maxevilmind@users.noreply.github.com>
2026-05-01 18:13:55 +0000

    deploy: ecb93a5949272159c6cc198a1cd3e8977f1d0155

--- chunks/pages-2ef3e4a8.js
+++ chunks/pages-2ef3e4a8.js
@@ -1,4 +1,4 @@
-import{i as e,a as t,x as r}from"./vendor-6907c12a.js";class AppButton extends e{static properties={href:{type:String},variant:{type:String},fullWidth:{type:Boolean,attribute:"full-width"},target:{type:String}};static styles=t`
+import{i as e,a as t,x as r}from"./vendor-8fc737ca.js";class AppButton extends e{static properties={href:{type:String},variant:{type:String},fullWidth:{type:Boolean,attribute:"full-width"},target:{type:String}};static styles=t`
     :host {
       display: inline-flex;
     }
@@ -243,7 +243,7 @@ import{i as e,a as t,x as r}from"./vendor-6907c12a.js";class AppButton extends e
       <custom-card title="About Me" subtitle="Senior Software Engineer at ING" hoverable>
         <div class="page-stack" style="gap: var(--space-4);">
           <p>
-            I'm a Senior Software Engineer with 7+ years of experience in JavaScript development, 
+            I'm a Senior Software Engineer with 8+ years of experience in JavaScript development, 
             currently working at ING in the Amsterdam Area. I specialize in building scalable 
             applications and solving complex problems.
           </p>
@@ -332,4 +332,4 @@ import{i as e,a as t,x as r}from"./vendor-6907c12a.js";class AppButton extends e
         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
       </svg>
     `}});
-//# sourceMappingURL=pages-f191624c.js.map
+//# sourceMappingURL=pages-2ef3e4a8.js.map
